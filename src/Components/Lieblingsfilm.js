37e892d7a864a2f2c4bb9af8bf0995a065732007
houra49 <houra.shamshyrcar@gmail.com>
2021-03-13 23:56:27 +0100

    lieblingsfilm

--- src/Components/Lieblingsfilm.js
+++ src/Components/Lieblingsfilm.js
@@ -4,9 +4,9 @@ const Lieblingsfilm = (props) => {
         <figure>
             <img src={props.img} alt="" />
             <figcaption>
-                <h2>{props.filmTitle}</h2>
-                <h2>{props.filmYear}</h2>
-                <h2>{props.filmDirector}</h2>
+                <h2 className="title">{props.filmTitle}</h2>
+                <h2 className="year">{props.filmYear}</h2>
+                <h2 className="director">{props.filmDirector}</h2>
             </figcaption>
         </figure>
     );
