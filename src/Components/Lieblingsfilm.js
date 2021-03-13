34a87e17bcf5cc24468de30a602031d8d1250b05
houra49 <houra.shamshyrcar@gmail.com>
2021-03-13 22:53:16 +0100

    lieblingsfilm

---
+++ src/Components/Lieblingsfilm.js
@@ -0,0 +1,14 @@
+const Lieblingsfilm = (props) => {
+    console.log(props)
+    return (
+        <figure>
+            <img src={props.img} alt="" />
+            <figcaption>
+                <h2>{props.filmTitle}</h2>
+                <h2>{props.filmYear}</h2>
+                <h2>{props.filmDirector}</h2>
+            </figcaption>
+        </figure>
+    );
+}
+export default Lieblingsfilm;
